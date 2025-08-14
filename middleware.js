699afd0087150f7fd60aa957d5586146e196218b
Marcus Little <mcolelittle@gmail.com>
2025-08-14 16:54:25 -0400

    remove middleware

--- middleware.js
+++ middleware.js
@@ -1,31 +1,31 @@
-// middleware.js
-import { NextResponse } from 'next/server'
+// // middleware.js
+// import { NextResponse } from 'next/server'
 
-async function getRedirectDestination(pathname) {
-  // This could fetch from a CMS, database, or static config
-  // For now, just using a simple object
-  const redirects = {
-    '/test': '/image-cdn',
-  }
+// async function getRedirectDestination(pathname) {
+//   // This could fetch from a CMS, database, or static config
+//   // For now, just using a simple object
+//   const redirects = {
+//     '/test': '/image-cdn',
+//   }
   
-  return redirects[pathname]
-}
+//   return redirects[pathname]
+// }
 
-export async function middleware(request) {
-  const { pathname, search } = request.nextUrl
-  const host = request.headers.get('host')
+// export async function middleware(request) {
+//   const { pathname, search } = request.nextUrl
+//   const host = request.headers.get('host')
   
-  const redirectTo = await getRedirectDestination(pathname)
+//   const redirectTo = await getRedirectDestination(pathname)
   
-  if (redirectTo) {
-    // Use the same protocol and host as the incoming request
-    const redirectUrl = `${request.nextUrl.protocol}//${host}${redirectTo}${search}`
-    return NextResponse.redirect(redirectUrl, 301)
-  }
+//   if (redirectTo) {
+//     // Use the same protocol and host as the incoming request
+//     const redirectUrl = `${request.nextUrl.protocol}//${host}${redirectTo}${search}`
+//     return NextResponse.redirect(redirectUrl, 301)
+//   }
   
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-export const config = {
-  matcher: '/:path*',
-}
+// export const config = {
+//   matcher: '/:path*',
+// }
